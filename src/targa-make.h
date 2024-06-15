11717108238392f89eb60d3469a38b00a1a89c21
cqing0 <boerbasher@protonmail.com>
2024-06-15 15:51:58 +0100

    Add write_tga func

--- src/targa-make.h
+++ src/targa-make.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdint.h>
 
 typedef struct TGAHead {
@@ -17,3 +18,24 @@ typedef struct TGAHead {
 	uint8_t pixel_depth;
 	uint8_t image_desc;
 };
+
+void write_tga(const char *filename, const uint8_t *pixel_data, int width, int height)
+{
+	FILE *file = fopen(filename "wb");
+	if (!file) {
+		perror("[ERROR]: Could not open TGA file\n");
+		return;
+	}
+
+	TGAHead tga_head = {0};
+	tga_head.image_type = 2;
+	tga_head.width = width;
+	tga_head.height = height;
+	tga_head.pixel_depth = 24;
+	tga_head.image_desc = 0x20;
+
+	fwrite(&tga_head, sizeof(TGAHead), 1, file);
+	fwrite(pixel_data, width * height * 3, 1, file);
+
+	fclose(file);
+}
